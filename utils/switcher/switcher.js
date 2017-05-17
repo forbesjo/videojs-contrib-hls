c1ec39fd38e676c60a69b9036957d20c82533630
Shan Zhuang <szhuang@szhuang-retina.vidmark.local>
2017-05-17 10:47:20 -0400

    load mulitple files

--- utils/switcher/switcher.js
+++ utils/switcher/switcher.js
@@ -4,6 +4,7 @@ import Config from '../../src/config';
 
 // a dynamic number of time-bandwidth pairs may be defined to drive the simulation
 let networkTimeline = document.querySelector('.network-timeline');
+let fileNetworkTrace = '';
 let $ = document.querySelector.bind(document);
 
 // apply any simulation parameters that were set in the fragment identifier
@@ -24,7 +25,8 @@ if (window.location.hash) {
 
 // collect the simulation parameters
 const parameters = function() {
-  let networkTrace = $('#network-trace').value
+  let value = fileNetworkTrace.length ? fileNetworkTrace : $('#network-trace').value;
+  let networkTrace = value
     .trim()
     .split('\n')
     .map((line) => line.split(' ').slice(-2).map(Number));
@@ -59,19 +61,24 @@ let local = $('#local');
 // clear the file path to allow for reload
 local.addEventListener('click', () => local.value = '');
 local.addEventListener('change', function() {
-  const [file] = local.files;
-  const reader = new FileReader();
+  $('#network-trace').style.display = 'none';
+  fileNetworkTrace = '';
+  const files = local.files;
 
   // do nothing if no file was chosen
-  if (!file) {
+  if (!files) {
     return;
   }
 
-  reader.addEventListener('loadend', function() {
-    $('#network-trace').value = reader.result;
-  });
+  for(var i = 0; i < files.length; i++) {
+    const reader = new FileReader();
 
-  reader.readAsText(file);
+    reader.addEventListener('loadend', function() {
+      fileNetworkTrace += reader.result;
+    });
+
+    reader.readAsText(files[i]);
+  }
 });
 
 let saveReport = $('#save-report');
